9d06642322fe615acf4d00f48fcb043eac2ddb74
Joselon <joselon1999@hotmail.com>
2020-03-19 22:15:59 -0500

    Implementacion de Eureka naming server y Zuul gateway

--- src/main/java/everis/bootcamp/bankAccountMicroservice/BankAccountMicroserviceApplication.java
+++ src/main/java/everis/bootcamp/bankAccountMicroservice/BankAccountMicroserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BankAccountMicroserviceApplication {
 
